c13a7c94712798bae4e3ae34209e4d4c5f4ab89e
Barry0501 <laptung051@gmail.com>
2020-12-28 12:26:27 +0700

    Update phone_auth_credential.dart

--- lib/src/auth_credential/phone_auth_credential.dart
+++ lib/src/auth_credential/phone_auth_credential.dart
@@ -1,16 +1,18 @@
 import 'auth_credential.dart';
 
 class PhoneAuthCredential extends AuthCredential {
-  PhoneAuthCredential({this.verifiedOTP}) : super(_url);
+  PhoneAuthCredential({this.code,this.phone}) : super(_url);
 
   static const String _url = '/login-otp';
 
   @override
   Map<String, String> asMap() {
     return {
-      'verified_otp_id': verifiedOTP,
+      'code': code,
+      'phone': phone
     };
   }
 
-  final String verifiedOTP;
+  final String code;
+  final String phone;
 }
